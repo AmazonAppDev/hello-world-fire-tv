f2e3d16aca3a98133d509201404c6e75654ce643
sungolivia <yoola93@gmail.com>
2024-10-03 16:12:23 -0700

    Upgrade androidx.tv to stable release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.tv:tv-foundation:1.0.0-alpha10")
+    implementation("androidx.tv:tv-foundation:1.0.0-alpha11")
     implementation("androidx.tv:tv-material:1.0.0-alpha10")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
